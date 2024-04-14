9ee9864b58a720849907e8961e9037181e7e0648
murielle Delmotte <murielle@thinkr.fr>
2024-04-14 13:28:53 +0200

    feat: add flexbox in css
    - Use in template_find_truffle.html

--- tests/testthat/test-utils_log_shiny.R
+++ tests/testthat/test-utils_log_shiny.R
@@ -1,10 +1,8 @@
 test_that("utils log shiny works", {
-  
-expect_type(info("fakeinfo"), "character")
-expect_type(danger("fakedanger"), "character")
-expect_type(rule("fakerule"), "character")
-expect_type(log_info_dev("fake"), "character")
-expect_type(log_danger_dev("fake"), "character")
-expect_type(log_rule_dev("fake"), "character")
-
+  expect_type(info("fakeinfo"), "character")
+  expect_type(danger("fakedanger"), "character")
+  expect_type(rule("fakerule"), "character")
+  expect_type(log_info_dev("fake"), "character")
+  expect_type(log_danger_dev("fake"), "character")
+  expect_type(log_rule_dev("fake"), "character")
 })
